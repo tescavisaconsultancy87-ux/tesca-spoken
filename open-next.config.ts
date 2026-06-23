0dcf1f422baa046c5c71558dc88ecf014bf28c7a
Avadh Dhameliya <dhameliyaavadh592@gmail.com>
2026-06-23 12:29:38 +0530

    build fix

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+// OpenNext Cloudflare adapter configuration
+// @opennextjs/cloudflare is installed during the Cloudflare build pipeline
+const config = {
+  default: {},
+};
+
+export default config;
